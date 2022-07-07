94cb19f72e84d8ef31a2f69deac169f1ae9fcaaf
unknown <abdulkarimansari2000@gmail.com>
2022-07-07 20:30:50 +0530

    first commit

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const port= process.env.PORT || 5000
 app.use(express.static(__dirname+'/statics'));
 
 app.get('/',(req,res)=>{
-    res.sendFile('/index.html')
+    res.sendFile(__dirname+'/index.html')
 });
 
 app.listen(port, ()=>console.log(`listening on ${port}`))
